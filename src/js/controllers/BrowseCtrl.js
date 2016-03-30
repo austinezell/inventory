a3c3ee1750bc2d2133d549c7e8b4d3d2b8808143
Austin Ezell <austin.r.ezell@gmail.com>
2016-03-29 17:04:25 -0700

    generates browse

--- src/js/controllers/BrowseCtrl.js
+++ src/js/controllers/BrowseCtrl.js
@@ -6,16 +6,26 @@
   function BrowseCtrl(InventoryService){
     let vm = this;
     vm.inventory = {};
-    getAll();
+    vm.filters = [{
+      name: "Office",
+      filter: "office"
+    }, {
+      name: "Food Service",
+      filter: "foodstuffs"
+    },{
+      name: "Household",
+      filter: "household"
+    }]
+      getAll();
 
-    function getAll(){
-      InventoryService.getAll()
-      .then(res=>{
-        vm.inventory.CH1 = res.data.CH1;
-        vm.inventory.CH2 = res.data.CH2;
-        
-      })
+      function getAll(){
+        InventoryService.getAll()
+        .then(res=>{
+          vm.inventory.CH1 = res.data.CH1;
+          vm.inventory.CH2 = res.data.CH2;
+
+        })
+      }
+
     }
-
-  }
-})()
+  })()
