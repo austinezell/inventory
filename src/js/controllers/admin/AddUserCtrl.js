f38ffde0fa9ad7b4870c20e9daaa917d752f7aaf
Austin Ezell <austin.r.ezell@gmail.com>
2016-03-29 23:21:30 -0700

    user email validation

--- src/js/controllers/admin/AddUserCtrl.js
+++ src/js/controllers/admin/AddUserCtrl.js
@@ -5,13 +5,24 @@
   AddUserCtrl.$inject = ["UserService", "$ionicPopup"]
 
   function AddUserCtrl(UserService, $ionicPopup){
+    let vm = this;
     vm.newUser ={};
+    vm.emailRegEx = /(\w+\.)*(\w+@\w+\.\w+)(\.\w+)*/
     vm.addUser = function(user) {
       UserService.addUser(user)
       .then(res=>{
         vm.newUser = {};
+        $ionicPopup.alert({
+          title: "Success!",
+          cssClass: "success",
+          template: "User successfully added."
+        })
       },err=>{
-        console.error(err);
+        $ionicPopup.alert({
+          title: "Error!",
+          cssClass: "error",
+          template: "Something went wrong. Call AJ."
+        })
       })
     }
   }
