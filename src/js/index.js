aef6a7b5d524c8907c44f39fa8502054b3d4e568
Austin Ezell <austin.r.ezell@gmail.com>
2016-03-29 17:49:16 -0700

    includes compiled index.js into git ignore

--- src/js/index.js
+++ src/js/index.js
@@ -5,5 +5,6 @@ import "./controllers/BrowseCtrl.js";
 import "./controllers/InvenCtrl.js";
 import "./controllers/InvenLogCtrl.js";
 import "./controllers/ItemCtrl.js";
+import "./controllers/OrderLogCtrl.js";
 import "./config/adminStates.js";
 import "./config/states.js";
