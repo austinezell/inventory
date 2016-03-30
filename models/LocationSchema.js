d9c816bc5ae78372b8fbd508a1ae2b6b33e1c2e3
Austin Ezell <austin.r.ezell@gmail.com>
2016-03-29 23:46:34 -0700

    removes unused location schema

--- models/LocationSchema.js
+++
@@ -1,9 +0,0 @@
-const Mongoose = require("mongoose");
-
-const LocationSchema = new Mongoose.Schema({
-  name: {type: String, enum: ["CH1", "CH2"], unique: true},
-  address: {type: String},
-  items: [{type: Mongoose.Schema.Types.ObjectId, ref: "Item"}]
-});
-
-module.exports = Mongoose.model("Location", LocationSchema);
